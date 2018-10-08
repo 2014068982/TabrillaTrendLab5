3c7f3e19321aa50804ddb8868f8a543763edf63e
2014068982 <42483262+2014068982@users.noreply.github.com>
2018-10-08 15:09:19 +0800

    LAB ACTIVITY 5
    'process' not working

---
+++ app/src/main/java/com/example/mclorenz/tabrillatrendlab5/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.mclorenz.tabrillatrendlab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void process (View v){
+        Intent i=null, chooser=null;
+        if (v.getId() == R.id.btn1){
+            i = new Intent(Intent.ACTION_RUN);
+            i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.btnMap1){
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.6336, 121.0194"));
+            chooser = Intent.createChooser(i, "Select application");
+            startActivity(chooser);
+        }
+    }
+}
